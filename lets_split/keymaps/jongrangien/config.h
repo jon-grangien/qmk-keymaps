ef797de71eb2090237c7afb52c17191dd58471b0
codingInSpace <jonathan.grangien@gmail.com>
2019-02-28 23:19:44 +0100

    Tweak some layers and set some configurable defines

--- lets_split/keymaps/jongrangien/config.h
+++ lets_split/keymaps/jongrangien/config.h
@@ -31,6 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 /* Select hand configuration */
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
+
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
+
