76bd5b232fcddeacb293da890e4d261638d4d87f
codingInSpace <jonathan.grangien@gmail.com>
2019-03-13 22:37:20 +0100

    switch shift key location

--- lets_split/keymaps/jongrangien/keymap.c
+++ lets_split/keymaps/jongrangien/keymap.c
@@ -47,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |TabSft|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Config| Ctrl | Alt  | GUI  |Lower |SftBsp|Space |Raise | RCTRL| C_TAB| RALT | Fn   |
+ * |Config| Ctrl | Alt  | GUI  |Lower | Bksp |SftSpc|Raise | RCTRL| C_TAB| RALT | Fn   |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,     KC_SCLN, KC_BSPC, \
-  CTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,    KC_N,    KC_E,    KC_I,     KC_O,    KC_QUOT, \
-  TAB_SHIFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_ENT , \
-  CONFIG,         KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SHIFT_BCKSPC, KC_SPC,  RAISE,   KC_RCTL,  CTRL_TAB, KC_RALT, FN  \
+  KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,       KC_L,    KC_U,    KC_Y,     KC_SCLN,  KC_BSPC, \
+  CTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,       KC_N,    KC_E,    KC_I,     KC_O,     KC_QUOT, \
+  TAB_SHIFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,       KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT , \
+  CONFIG,         KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC,      SHIFT_SPC,  RAISE,   KC_RCTL,  CTRL_TAB, KC_RALT, FN  \
 ),
 
 /* Qwerty
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |TabSft|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Config| Ctrl | Alt  | GUI  |Lower |SftBsp|Space |Raise | RCTRL|C_TAB | RALT | Fn   |
+ * |Config| Ctrl | Alt  | GUI  |Lower | Bksp |SftSpc|Raise | RCTRL|C_TAB | RALT | Fn   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
-   CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, \
-   TAB_SHIFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_ENT , \
-   CONFIG,        KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SHIFT_BCKSPC, KC_SPC,  RAISE,   KC_RCTL,  CTRL_TAB, KC_RALT, FN  \
+   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,        KC_BSPC, \
+   CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,     KC_QUOT, \
+   TAB_SHIFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,     KC_ENT , \
+   CONFIG,         KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC,      SHIFT_SPC,  RAISE,   KC_RCTL,  CTRL_TAB, KC_RALT, FN  \
 ),
 
 /* Lower
