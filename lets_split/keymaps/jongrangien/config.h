a71d9394df84979ffb1b0a7a5526e88ca41b7bdd
codingInSpace <jonathan.grangien@gmail.com>
2019-04-03 07:45:21 +0200

    Decrease tapping term

--- lets_split/keymaps/jongrangien/config.h
+++ lets_split/keymaps/jongrangien/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 #define EE_HANDS
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 100
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 
