eec18819eb8220f4e47e509d35cfa4808768b1bb
codingInSpace <jonathan.grangien@gmail.com>
2019-10-31 22:33:54 +0100

    Change shift-tab to hyper-tab

--- lets_split/keymaps/jongrangien/keymap.c
+++ lets_split/keymaps/jongrangien/keymap.c
@@ -37,7 +37,7 @@ enum custom_keycodes {
 #define SHFT_ALT_TAB   LSFT(LALT(KC_TAB))
 #define ALT_F4         LALT(KC_F4)
 #define GUI_TAB        LGUI(KC_TAB)
-#define TAB_SHIFT      SFT_T(KC_TAB)
+#define HYPER_TAB      HYPR_T(KC_TAB)
 #define L_AG_SWAP      MAGIC_SWAP_LALT_LGUI
 #define L_AG_NORM      MAGIC_UNSWAP_LALT_LGUI
 #define PERM_ALT_TAB   LALT(LCTL(KC_TAB))
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|         |------+------+------+------+------+------|
  * |CrlEsc|   A  |   R  |   S  |   T  |   G  |         |   M  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|         |------+------+------+------+------+------|
- * |TabSft|   Z  |   X  |   C  |   D  |   V  |         |   K  |   H  |   ,  |   .  |   /  |R ctl |
+ * |HypTab|   Z  |   X  |   C  |   D  |   V  |         |   K  |   H  |   ,  |   .  |   /  |R ctl |
  * |------+------+------+------+------+------|         |------+------+------+------+------+------|
  * |Config| GUI  | Alt  |SC Sft|Lower | Bksp |         |Space |Raise |SC Sft| Enter| RALT | Fn   |
  * `-----------------------------------------'         '-----------------------------------------'
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_ortho_4x12( \
   KC_LGUI,       KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,    /**/   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
   CTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,  KC_G,    /**/   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  TAB_SHIFT,     KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,    /**/   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
+  HYPER_TAB,     KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,    /**/   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
   CONFIG,        KC_LGUI, KC_LALT, KC_LSPO, LOWER, KC_BSPC, /**/   KC_SPC, RAISE, KC_RSPC, KC_ENT, KC_RALT, FN  \
 ),
 
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |CrlEsc|   A  |   S  |   D  |   F  |   G  |          |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |TabSft|   Z  |   X  |   C  |   V  |   B  |          |   N  |   M  |   ,  |   .  |   /  |R ctl |
+ * |HypTab|   Z  |   X  |   C  |   V  |   B  |          |   N  |   M  |   ,  |   .  |   /  |R ctl |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |Config| GUI  | Alt  |SC Sft|Lower | Bksp |          |Space |Raise |SC Sft| Enter| RALT | Fn   |
  * `-----------------------------------------'          '-----------------------------------------'
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
    KC_LGUI,       KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    /**/   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
    CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    /**/   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   TAB_SHIFT,     KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    /**/   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
+   HYPER_TAB,     KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    /**/   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
    CONFIG,        KC_LGUI, KC_LALT, KC_LSPO, LOWER, KC_BSPC, /**/   KC_SPC, RAISE, KC_RSPC, KC_ENT, KC_RALT, FN  \
 ),
 
