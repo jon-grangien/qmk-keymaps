31621991181ebb7776a9f1aab2acf7587a6cd52b
codingInSpace <jonathan.grangien@gmail.com>
2020-02-25 22:49:35 +0100

    Make some cool changes

--- lets_split/keymaps/jongrangien/keymap.c
+++ lets_split/keymaps/jongrangien/keymap.c
@@ -16,6 +16,8 @@
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
+  NPM_START,
+  LSLAXH,
   GIT_ADD_ALL,
   GIT_PULL,
   GIT_COMMIT,
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak
  * ,-----------------------------------------.         .-----------------------------------------.
- * | GUI  |   Q  |   W  |   F  |   P  |   B  |         |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * | GUI  |   Q  |   W  |   F  |   P  |   B  |         |   J  |   L  |   U  |   Y  |   ;  | Del  |
  * |------+------+------+------+------+------|         |------+------+------+------+------+------|
  * |CrlEsc|   A  |   R  |   S  |   T  |   G  |         |   M  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|         |------+------+------+------+------+------|
@@ -59,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'         '-----------------------------------------'
  */
 [_COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_LGUI,       KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,    /**/   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
+  KC_LGUI,       KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,    /**/   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_DEL, \
   CTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,  KC_G,    /**/   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, \
   HYPER_TAB,     KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,    /**/   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
   CONFIG,        KC_LGUI, KC_LALT, KC_LSPO, LOWER, KC_BSPC, /**/   KC_SPC, RAISE, KC_RSPC, KC_ENT, KC_RALT, FN  \
@@ -67,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------.          .-----------------------------------------.
- * | GUI  |   Q  |   W  |   E  |   R  |   T  |          |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | GUI  |   Q  |   W  |   E  |   R  |   T  |          |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |CrlEsc|   A  |   S  |   D  |   F  |   G  |          |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
@@ -77,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'          '-----------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_LGUI,       KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    /**/   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+   KC_LGUI,       KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    /**/   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL, \
    CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    /**/   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
    HYPER_TAB,     KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    /**/   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL , \
    CONFIG,        KC_LGUI, KC_LALT, KC_LSPO, LOWER, KC_BSPC, /**/   KC_SPC, RAISE, KC_RSPC, KC_ENT, KC_RALT, FN  \
@@ -85,45 +87,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------.          .-----------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |          |      |   7  |  8   |  9   |  \   | Del  |
+ * |      |   !  |   @  |   #  |   $  |   %  |          |   =  |   7  |  8   |  9   |  \   |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |      |   ^  |   &  |   *  |   ~  |   `  |          |   |  |   4  |  5   |  6   |   +  |      |
+ * |      |   ^  |   &  |   *  |   ~  |   `  |          |   +  |   4  |  5   |  6   |   |  |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |      |      |      |      |  _   |      |          |      |   1  |  2   |  3   |   -  |      |
+ * |      |      |      |      |  _   |      |          |   -  |   1  |  2   |  3   |      |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |      |      |      |      |      |      |          |Enter |0/Rse |  .   |      |      |PRTSCR|
+ * |      |      |      |      |      |      |          |Enter |  Rse |  0   |  .   |      |PRTSCR|
  * `-----------------------------------------'          '-----------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,  /**/ _______, KC_7,       KC_8,   KC_9,    KC_BSLS, KC_DEL, \
-  _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_TILDE, KC_GRAVE, /**/ KC_PIPE, KC_4,       KC_5,   KC_6,    KC_PLUS, _______, \
-  _______, _______, _______, _______, KC_UNDS,  _______,  /**/ _______, KC_1,       KC_2,   KC_3,    KC_MINS, _______, \
-  _______, _______, _______, _______, _______,  _______,  /**/ KC_ENT,  ZERO_RAISE, KC_DOT, _______, _______, KC_PSCR \
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,  /**/ KC_EQUAL, KC_7,  KC_8, KC_9,   KC_BSLS, _______, \
+  _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_TILDE, KC_GRAVE, /**/ KC_PLUS,  KC_4,  KC_5, KC_6,   KC_PIPE, _______, \
+  _______, _______, _______, _______, KC_UNDS,  _______,  /**/ KC_MINS,  KC_1,  KC_2, KC_3,   _______, _______, \
+  _______, _______, _______, _______, _______,  _______,  /**/ KC_ENT,   RAISE, KC_0, KC_DOT, _______, KC_PSCR \
 ),
 
 /* Raise
  * ,-----------------------------------------.          .-----------------------------------------.
- * |      |   [  |  (   |  )   |  ]   |      |          |      |      |   =  |  -   |      |      |
+ * |      |   [  |  (   |  )   |  ]   |      |          |      |ALT_TB|S_A_TB|A_C_TB|GUITAB|      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |      |      |  ä   |  å   |  ö   |      |          |      |  <   |  v   |  ^   |   >  |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |      |      |      |      |      |      |          |CS_TAB|C_TAB |W_WS_L|W_WS_R|      |      |
+ * |      |      |      |      |      |      |          |      |CS_TAB|C_TAB |W_WS_L|W_WS_R|      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |      |      |      |      |      |      |          |      |      |      |      |      |      |
  * `-----------------------------------------'          '-----------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  _______, KC_LBRC, KC_LPRN,    KC_RPRN, KC_RBRC,       _______, /**/  _______,    _______,  KC_EQUAL,    KC_MINS,      _______,  _______, \
-  _______, _______, RALT(KC_Q), RALT(KC_W), RALT(KC_P), _______, /**/  _______,    KC_LEFT,  KC_DOWN,     KC_UP,        KC_RIGHT, _______, \
-  _______, _______, _______,    _______,    _______,    _______, /**/  CTRL_S_TAB, CTRL_TAB, WIN_WS_LEFT, WIN_WS_RIGHT, _______,  _______,   \
-  _______, _______, _______,    _______,    _______,    _______, /**/  _______,    _______,  _______,     _______,      _______,  _______ \
+  _______, KC_LBRC, KC_LPRN,    KC_RPRN, KC_RBRC,       _______, /**/  _______, ALT_TAB,    SHFT_ALT_TAB, PERM_ALT_TAB, GUI_TAB,      _______, \
+  _______, _______, RALT(KC_Q), RALT(KC_W), RALT(KC_P), _______, /**/  _______, KC_LEFT,    KC_DOWN,      KC_UP,        KC_RIGHT,     _______, \
+  _______, _______, _______,    _______,    _______,    _______, /**/  _______, CTRL_S_TAB, CTRL_TAB,     WIN_WS_LEFT,  WIN_WS_RIGHT, _______, \
+  _______, _______, _______,    _______,    _______,    _______, /**/  _______, _______,     _______,     _______,      _______,      _______ \
 ), 
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------.          .-----------------------------------------.
  * |      |      |      |      |      |      |          |      |      |      |      |      |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
- * |      |GUITAB|A_C_TB|S_A_TB|ALT_TB|      |          |      | HOME | PGDN | PGUP |  END |      |
+ * |      |      |      |npm s |  ls  |      |          |      | HOME | PGDN | PGUP |  END |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
  * |g push|g pull|g add |g comt|g sts |g diff|          |      |      |      |      |      |      |
  * |------+------+------+------+------+------|          |------+------+------+------+------+------|
@@ -131,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'          '-----------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______,  _______, _______,      _______,      _______,    _______,  /**/ _______, _______, _______, _______, _______, _______, \
-  _______,  GUI_TAB, PERM_ALT_TAB, SHFT_ALT_TAB, ALT_TAB,    _______,  /**/ _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, \
-  GIT_PUSH, GIT_PULL,GIT_ADD_ALL,  GIT_COMMIT,   GIT_STATUS, GIT_DIFF, /**/ _______, _______, _______, _______, _______, _______, \
-  _______,  _______, _______,      _______,      _______,    _______,  /**/ _______, _______, _______, _______, _______, _______ \
+  _______,  _______, _______,     _______,    _______,    _______,  /**/ _______, _______, _______, _______, _______, _______, \
+  _______,  _______, _______,     NPM_START,  LSLAXH,     _______,  /**/ _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, \
+  GIT_PUSH, GIT_PULL,GIT_ADD_ALL, GIT_COMMIT, GIT_STATUS, GIT_DIFF, /**/ _______, _______, _______, _______, _______, _______, \
+  _______,  _______, _______,     _______,    _______,    _______,  /**/ _______, _______, _______, _______, _______, _______ \
   ),
 
 /* Fn (F-keys etc)
@@ -189,6 +191,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case COLEMAK:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+    case NPM_START:
+      if (record->event.pressed) {
+        SEND_STRING("npm start");
+      }
+      return false;
+      break;
+    case LSLAXH:
+      if (record->event.pressed) {
+        SEND_STRING("ls -laXh");
       }
       return false;
       break;
